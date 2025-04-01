d105677cbb0045aedd66b9f023baf90dcec268dd
RomainBeche <134644974+RomainBeche@users.noreply.github.com>
2025-04-01 14:25:27 +0200

    Initial Commit

---
+++ RestThoseEyes/RestThoseEyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestThoseEyesApp.swift
+//  RestThoseEyes
+//
+//  Created by Romain Bêche on 01/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct RestThoseEyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RestThoseEyesTests/RestThoseEyesTests.swift
@@ -0,0 +1,17 @@
+//
+//  RestThoseEyesTests.swift
+//  RestThoseEyesTests
+//
+//  Created by Romain Bêche on 01/04/2025.
+//
+
+import Testing
+@testable import RestThoseEyes
+
+struct RestThoseEyesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
